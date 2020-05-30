0df789f6ac066d796827b280eda1e82c2288373a
Happyseaweed <32279381+Happyseaweed@users.noreply.github.com>
2020-05-30 10:48:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function realtimeClock(){ 
+    var rtClock = new Date();
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+    //am or pm
+    var amPm = (hours < 12) ? "AM" : "PM";
+
+    //convert into 12-hour format;
+    hours = (hours > 12) ? hours-12 : hours;
+
+    //leading zeros
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    //implement
+    document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds + " " + amPm;
+    var t = setTimeout(realtimeClock, 500);
+}
